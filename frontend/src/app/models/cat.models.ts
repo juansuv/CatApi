07f2f20bb89a5c71a0cd6f634a55eb1641ba0888
juan suarez <juan.suva25@gmail.com>
2025-09-10 23:34:37 -0500

    agrega modelo para los gatos, este contiene las interfaces

---
+++ frontend/src/app/models/cat.models.ts
@@ -0,0 +1,43 @@
+export interface CatBreed {
+  id: string;
+  name: string;
+  description: string;
+  temperament: string;
+  origin: string;
+  life_span: string;
+  weight: {
+    imperial: string;
+    metric: string;
+  };
+  adaptability: number;
+  affection_level: number;
+  child_friendly: number;
+  dog_friendly: number;
+  energy_level: number;
+  grooming: number;
+  health_issues: number;
+  intelligence: number;
+  shedding_level: number;
+  social_needs: number;
+  stranger_friendly: number;
+  vocalisation: number;
+  experimental: number;
+  hairless: number;
+  natural: number;
+  rare: number;
+  rex: number;
+  suppressed_tail: number;
+  short_legs: number;
+  wikipedia_url?: string;
+  hypoallergenic: number;
+  reference_image_id?: string;
+  image?: CatImage;
+}
+
+export interface CatImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  breeds?: CatBreed[];
+}
